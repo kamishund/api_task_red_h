2d2ad32c9db8a2687ad799a99f99833eca0c3072
kamishund <kamishund@gmail.com>
2021-09-19 13:48:31 +0900

    initial commit heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+
+web: gunicorn api_task_red.wsgi --log-file -
--- api_task_red/wsgi.py
+++ api_task_red/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
 
+import os
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_task_red.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
