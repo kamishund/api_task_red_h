790a924dc693dbcd347adac43e6b9118e58b695e
kamishund <kamishund@gmail.com>
2021-09-19 14:51:47 +0900

    make it better

--- api_task_red/settings.py
+++ api_task_red/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ["git.heroku.com/red-task.git"]
+ALLOWED_HOSTS = ["red-task.herokuapp.com"]
 
 
 # Application definition
